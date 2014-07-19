b71379170f67cf4d035e8ec75ad001fc13e4439d
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-07-19 16:46:03 +0000

    Add 2 more image

--- shot.sh
+++ shot.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 gnome-screenshot --delay=4 --file=/data/1.png --display=:1
+gnome-screenshot --delay=6 --file=/data/2.png --display=:1
+gnome-screenshot --delay=8 --file=/data/3.png --display=:1
