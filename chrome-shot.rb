8b1d9cf240cc3025fbbd6e02e5146b7ddb032a59
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-07-20 23:36:54 +0000

    Add killing chrome

--- chrome-shot.rb
+++ chrome-shot.rb
@@ -13,3 +13,12 @@ FileUtils.mkdir_p '/data/web/public'
 `gnome-screenshot --delay=1 --file=/data/web/public/1.png --display=:1`
 `gnome-screenshot --delay=1 --file=/data/web/public/2.png --display=:1`
 `gnome-screenshot --delay=1 --file=/data/web/public/3.png --display=:1`
+
+
+#kill chrome
+#(killing #fork's pid does not work)
+
+processes = `ps aux`.split("\n")
+google = processes.select{|process| process.include? 'google'}.first.split[1].to_i
+Process.kill 9, google
+
