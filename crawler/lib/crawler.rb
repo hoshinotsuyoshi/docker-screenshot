ca3e4e19a5fee389fb22e24a0be46e034944c9ab
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-07-23 07:05:30 +0900

    Add interval

--- crawler/lib/crawler.rb
+++ crawler/lib/crawler.rb
@@ -2,11 +2,14 @@
 
 class Crawler
   LIMIT = Float::INFINITY
+  INTERVAL = 60
+
   def self.Start
     @count = 0
     while LIMIT > @count
       Process.fork{}
       @count += 1
+      sleep INTERVAL
     end
   end
 end
--- crawler/spec/crawler_spec.rb
+++ crawler/spec/crawler_spec.rb
@@ -4,7 +4,8 @@ describe Crawler do
   describe '.Start' do
     context 'when LIMIT is 3' do
       it 'calls Process.fork 3 times' do
-        stub_const('Crawler::LIMIT',3)
+        stub_const('Crawler::INTERVAL', 0)
+        stub_const('Crawler::LIMIT', 3)
         expect(Process).to receive(:fork).exactly(3)
         Crawler.Start
       end
