ce62c87bc2da8ff35ebe014b58d7c236c85a6ac7
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-07-24 00:18:34 +0900

    Fix example description

--- crawler/spec/crawler_spec.rb
+++ crawler/spec/crawler_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Crawler do
   describe '.Start' do
     context 'when LIMIT is 3' do
-      it 'calls Crawler#run 3 times' do
+      it 'calls Crawler.run 3 times' do
         stub_const('Crawler::INTERVAL', 0)
         stub_const('Crawler::LIMIT', 3)
         allow(Crawler).to receive(:run).exactly(3)
