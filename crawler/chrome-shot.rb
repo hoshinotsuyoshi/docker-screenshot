3e6a369b6e8df42b9591e999ddc10524701fb02f
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-07-29 08:24:29 +0900

    Fix chrome-shot.rb

--- crawler/chrome-shot.rb
+++ crawler/chrome-shot.rb
@@ -15,20 +15,23 @@ sleep 12
 FileUtils.mkdir_p '/data/web/public'
 escaped = CGI.escape ARGV.first
 dir     = "/data/web/public/#{escaped}"
-FileUtils.mkdir_p dir
 
-3.times do |i|
-  `gnome-screenshot --delay=1 --file=#{dir}/#{i}.png --display=:1`
+if !File.exist? dir
+  FileUtils.mkdir_p dir
 
-  loop do
-    if File.exist? "#{dir}/#{i}.png"
-      image = Magick::Image.read("#{dir}/#{i}.png").first
-      image.crop!(Magick::NorthWestGravity,width=823,height=638)
-      image.crop!(Magick::SouthEastGravity,width=640,height=350)
-      image.write "#{dir}/#{i}.png"
-      break
+  3.times do |i|
+    `gnome-screenshot --delay=1 --file=#{dir}/#{i}.png --display=:1`
+
+    loop do
+      if File.exist? "#{dir}/#{i}.png"
+        image = Magick::Image.read("#{dir}/#{i}.png").first
+        image.crop!(Magick::NorthWestGravity,width=823,height=638)
+        image.crop!(Magick::SouthEastGravity,width=640,height=350)
+        image.write "#{dir}/#{i}.png"
+        break
+      end
+      sleep 1
     end
-    sleep 1
   end
 end
 
